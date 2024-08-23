a3b9c26609b863a4e587ab8b492ab8eede201399
Alex <connectionwithme1@gmail.com>
2024-08-23 15:32:31 +0300

    Notes on upcoming work:
     - creating Notes functionality
     - creating Database class + required content
     - add file config.php
     - display all Notes posts
     - add extension to class Database(get, find, findOrFail)
     - display single Note post
     - add function Abort in file functions.php
     - add function authorize
     - add file 403

--- views/about.view.php
+++ views/about.view.php
@@ -8,8 +8,8 @@ require base_path("views/partials/banner.php");
 
 <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        Hello, this is Home Page
+        Hello, this is About Us Page
     </div>
 </main>
 
-
+<?php require base_path("views/partials/footer.php"); ?>
