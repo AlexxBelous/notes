600d9264e81082a336f166ec492aa964bdd8388b
AlexxBelous <connectionwithme1@gmail.com>
2024-08-26 06:25:03 +0300

    Notes on upcoming work:
     - creating functionality for update notes:
       - creating functionality to call the edit form
       - for file edit.php:
           -get note from database by id
           -check assess rights(user matching)
           -display note edit page
       - creating functionality patch request notes:
           -find the corresponding note
           -authorize that the current user can edit the note
           -validate the form
           -if no validation errors, update the record in the notes database table.

---
+++ controllers/notes/destroy.php
@@ -0,0 +1,20 @@
+<?php
+use Core\Database;
+use Core\App;
+
+$currentUserId = 1;
+
+$db = App::resolve(Database::class);
+
+$note = $db->query('select * from notes where id = :id', [
+    'id' => $_POST['id']
+])->findOrFail();
+
+
+authorize($note['user_id'] === $currentUserId);
+
+$db->query('delete from notes where id = :id', [
+    'id' => $_POST['id']
+]);
+header('location: /notes');
+
