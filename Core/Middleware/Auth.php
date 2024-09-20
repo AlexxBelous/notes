bedb0fd3f3c9aeb0857c769d6e96a9b7647b5034
Alex <connectionwithme1@gmail.com>
2024-09-20 16:13:12 +0300

    Notes on upcoming work:
     - refactoring the project structure(add Http Forms)
     - creating functionality log in(routers, form)
     - set Log In form
     - set Log In functionality(routers, store.php)
     - create class LoginForm + required content
     - create class Authenticator + method log in and log out

---
+++ Core/Middleware/Auth.php
@@ -0,0 +1,13 @@
+<?php
+namespace Core\Middleware;
+
+class Auth
+{
+    public static function handle()
+    {
+        if(!isset($_SESSION['user'])) {
+            header('location: /');
+            exit();
+        }
+    }
+}
